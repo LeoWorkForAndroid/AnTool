81b930398f22586ddc5f0eccd97fb50bcf48fba6
leo <liyang.ok@outlook.com>
2019-05-26 08:41:47 +0800

    first init

---
+++ app/src/main/java/com/top/antool/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.top.antool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.top.tupdatelib.TUpdate
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
