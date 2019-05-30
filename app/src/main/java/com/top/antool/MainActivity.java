44e2356cce6d3a905c3f752461a0257089f15e0f
liyang <liyang@corewise.cn>
2019-05-30 14:37:24 +0800

    VoiceLineView

--- app/src/main/java/com/top/antool/MainActivity.java
+++ app/src/main/java/com/top/antool/MainActivity.java
@@ -1,21 +1,33 @@
 package com.top.antool;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.media.audiofx.Visualizer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.ActivityCompat;
 import com.top.antoollib.ui.VoiceLineView;
 
 import java.io.*;
+import java.util.logging.Logger;
 
-public class MainActivity extends AppCompatActivity implements Runnable{
+public class MainActivity extends AppCompatActivity implements Runnable {
 
     private String TAG = "VoiceLineView";
 
@@ -40,17 +52,32 @@ public class MainActivity extends AppCompatActivity implements Runnable{
     private AudioRecord mAudioRecord;//创建AudioRecorder对象
     private VoiceLineView voiceLineView;
     private AppCompatButton button;
-    private boolean isRecording=false;
+    private boolean isRecording = false;
 
-    private File mFileRoot=null;//文件目录
+    private File mFileRoot = null;//文件目录
     //存放的目录路径名称
-    private static final String mPathName = Environment.getExternalStorageDirectory().getAbsolutePath()+ "/AudiioRecordFile";
+    private static final String mPathName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/AudiioRecordFile";
 
     private static final String mFileName = "audiorecordtest.pcm";
 
     //缓冲区中数据写入到数据，因为需要使用IO操作，因此读取数据的过程应该在子线程中执行。
     private Thread mThread;
+    Visualizer.OnDataCaptureListener dataCaptureListener;
+    Visualizer visualizer = null;
 
+    private static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.MODIFY_AUDIO_SETTINGS,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+
+    private static final int REQUEST_RECORD_AUDIO = 0X01;
+    private static final int REQUEST_READ_STORAGE = 0x01;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,19 +85,29 @@ public class MainActivity extends AppCompatActivity implements Runnable{
 
         voiceLineView = (VoiceLineView) findViewById(R.id.voicLine);
 
-        button=findViewById(R.id.btn);
+        button = findViewById(R.id.btn);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_STORAGE);
+            }
+            if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
+            }
+        }
+
 
         mFileRoot = new File(mPathName);
-        if(!mFileRoot.exists())
+        if (!mFileRoot.exists())
             mFileRoot.mkdirs();//创建文件夹
 
         button.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()){
+                switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         button.setText("Recording");
-                       startRecord();
+                        startRecord();
                         return true;
                     case MotionEvent.ACTION_UP:
                         button.setText("录音");
@@ -80,30 +117,70 @@ public class MainActivity extends AppCompatActivity implements Runnable{
                 return false;
             }
         });
-
         mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, mSampleRateInHz, mChannelConfig, mAudioFormat, mBufferSizeInBytes);
+        dataCaptureListener = new Visualizer.OnDataCaptureListener() {
+            @Override
+            public void onWaveFormDataCapture(Visualizer visualizer, byte[] waveform, int samplingRate) {
 
-
+            }
+            @Override
+            public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
+                //fft
+                Log.i(TAG, "fft length: " + fft.length);
+                for (int i = 0; i < fft.length; i += 2) {
+                    float magnitude = (float) Math.hypot(fft[i], fft[i + 1]);
+                    float phase = (float) Math.atan2(fft[i + 1], fft[i]);
+                }
+            }
+        };
     }
 
     //开始录音
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     public void startRecord() {
 
         //AudioRecord.getMinBufferSize的参数是否支持当前的硬件设备
         if (AudioRecord.ERROR_BAD_VALUE == mBufferSizeInBytes || AudioRecord.ERROR == mBufferSizeInBytes) {
             throw new RuntimeException("Unable to getMinBufferSize");
-        }else{
+        } else {
             destroyThread();
             isRecording = true;
-            if(mThread == null){
+            if (mThread == null) {
                 mThread = new Thread(this);
                 mThread.start();//开启线程
             }
+        }
+
+        try {
+            int mediaPlayerId = mAudioRecord.getAudioSessionId();
+            if (visualizer != null) {
+                visualizer.release();
+            }
+            visualizer = new Visualizer(mediaPlayerId);
+
+
+            /**
+             *可视化数据的大小： getCaptureSizeRange()[0]为最小值，getCaptureSizeRange()[1]为最大值
+             *
+             */
+            int captureSize = Visualizer.getCaptureSizeRange()[1];
+            int captureRate = Visualizer.getMaxCaptureRate() * 3 / 4;
+
+            visualizer.setCaptureSize(captureSize);
+            visualizer.setDataCaptureListener(dataCaptureListener, captureRate, true, true);
+            visualizer.setScalingMode(Visualizer.SCALING_MODE_NORMALIZED);
+            visualizer.setEnabled(true);
+        } catch (Exception e) {
+            Log.e(TAG, "请检查录音权限"+e.toString());
         }
     }
 
     //停止录音
     public void stopRecord() {
+        visualizer.setEnabled(false);
+        if (visualizer != null) {
+            visualizer.release();
+        }
         isRecording = false;
         //停止录音，回收AudioRecord对象，释放内存
         if (mAudioRecord != null) {
@@ -139,20 +216,40 @@ public class MainActivity extends AppCompatActivity implements Runnable{
     }
 
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_READ_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                finish();
+            }
+        }
+        if (requestCode == REQUEST_RECORD_AUDIO) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                finish();
+            }
+        }
+    }
+
+
     @Override
     public void run() {
         //标记为开始采集状态
         isRecording = true;
         //创建一个流，存放从AudioRecord读取的数据
         File mRecordingFile = new File(mFileRoot, mFileName);
-        if(mRecordingFile.exists()){//音频文件保存过了删除
+        if (mRecordingFile.exists()) {//音频文件保存过了删除
             mRecordingFile.delete();
         }
         try {
             mRecordingFile.createNewFile();//创建新文件
         } catch (IOException e) {
             e.printStackTrace();
-            Log.e("lu","创建储存音频文件出错");
+            Log.e(TAG, "创建储存音频文件出错");
         }
 
         try {
@@ -162,10 +259,10 @@ public class MainActivity extends AppCompatActivity implements Runnable{
             mAudioRecord.startRecording();//开始录音
             //getRecordingState获取当前AudioReroding是否正在采集数据的状态
             while (isRecording && mAudioRecord.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
-                int bufferReadResult = mAudioRecord.read(buffer,0,mBufferSizeInBytes);
-                for (int i = 0; i < bufferReadResult; i++)
-                {
+                int bufferReadResult = mAudioRecord.read(buffer, 0, mBufferSizeInBytes);
+                for (int i = 0; i < bufferReadResult; i++) {
                     mDataOutputStream.write(buffer[i]);
+
                 }
             }
             mDataOutputStream.close();
