e4de0512012d6e69d7577f928a11e5caa70f7e1d
leo <liyang.ok@outlook.com>
2019-07-09 23:00:06 +0800

    first init

--- app/src/main/java/com/top/antool/kerasActivity.java
+++ app/src/main/java/com/top/antool/kerasActivity.java
@@ -7,18 +7,12 @@ import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 public class kerasActivity extends AppCompatActivity {
 
-
     private TensorFlowInferenceInterface tf;
-
     private String MODEL_PATH = "file:///android_asset/squeezenet.pb";
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         tf=new TensorFlowInferenceInterface(getAssets(),MODEL_PATH);
-
-
 
     }
 }
